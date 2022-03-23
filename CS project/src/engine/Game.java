d7fe0ce42c9e734a6553a33acbc38b1ebd2dab65
Rowan Hegazy <Rowan Hegazy@LAPTOP-7U1ULFBR>
2022-03-23 17:14:08 +0200

    Game class updates missing csv file reads.

--- CS project/src/engine/Game.java
+++ CS project/src/engine/Game.java
@@ -2,6 +2,8 @@ package engine;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
+
 import model.abilities.*;
 import model.world.*;
 
@@ -65,16 +67,44 @@ public class Game {
 	}
 
 	private void placeChampions() {
-		
+		 ArrayList<Champion> p1=firstPlayer.getTeam();
+		 ArrayList<Champion> p2=secondPlayer.getTeam();
+		 board=new Object[BOARDWIDTH][BOARDHEIGHT];
+		 for(int i = 1; i<4;i++){
+			 board[0][i] = p1.get(i-1);
+		 }
+		 for(int i = 1; i<4;i++){
+			 board[4][i] = p2.get(i-1);
+		 }
 	}
 	
 	private void placeCovers() {
-		
+		int c = 0;
+		while(c!=5){
+			Random r = new Random();
+			int x = r.nextInt(3)+1;
+			int y = r.nextInt(5);
+			if(board[x][y] == null){
+				board[x][y] = new Cover(x, y);
+				c++;
+			}
+		}		
 	}
 	
 	public static void loadAbilities(String filePath) throws Exception {
 		BufferedReader br= new BufferedReader(new FileReader("Abilities.csv"));
-		availableChampions=new ArrayList<>();
+		availableAbilities=new ArrayList<Ability>();
+		String[] a = new String[9];
+				
+		if(a[0].equals("DMG")){
+			availableAbilities.add(new DamagingAbility(a[1], Integer.parseInt(a[2]), 
+					Integer.parseInt(a[4]), Integer.parseInt(a[3]), AreaOfEffect.valueOf(a[5]),Integer.parseInt(a[6]),Integer.parseInt(a[7])));
+		}
+		if(a[0].equals("HEL")){
+			availableAbilities.add(new HealingAbility(a[1], Integer.parseInt(a[2]), 
+					Integer.parseInt(a[4]), Integer.parseInt(a[3]), AreaOfEffect.valueOf(a[5]),Integer.parseInt(a[6]),Integer.parseInt(a[7])));
+		}
+		
 	}
 	
 	public static void loadChampions(String filePath) throws Exception {
